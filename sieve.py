9aa4e4ede64f987dc0ba1da6c58139ad9079b616
jdlowman2 <jdlowman2@gmail.com>
2018-01-12 19:45:20 -0800

    Add files via upload
    adding main python file

---
+++ sieve.py
@@ -0,0 +1,30 @@
+import math
+import sys
+
+def main(n):
+    primes = find_primes(n)
+    print("Primes are ", primes[:])
+    
+def find_primes(n):
+    if type(n) == float:
+        print("Warning: float given. Mapping input to integer")
+    n = int(n)
+    primes = range(2, n)
+    # this is a brute force way
+    # there are some integers i which can be skipped 
+    # (i.e. multiples of an integer that have already been tested)
+    for i in range(2, n):
+        if i in primes:
+            for p, num in enumerate(primes):
+                if num % i ==0 and num != i:
+                    primes.pop(p)
+    
+    return primes
+    
+if __name__=="__main__":
+    if len(sys.argv) > 1:
+        n = sys.argv[1]
+        main(n)
+    else:
+        n = input("I will find all the primes less than ...: ")
+        main(n)
